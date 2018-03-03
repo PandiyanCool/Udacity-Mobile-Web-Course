8fb691647e35c4ef5feafe7733e94aeea885a97a
Pandiyan <pandiyanit7@gmail.com>
2018-03-03 17:48:25 +0530

    service workers request hacks

--- wittr/public/js/sw/index.js
+++ wittr/public/js/sw/index.js
@@ -1,10 +1,38 @@
 self.addEventListener('fetch', function (event) {
-event.respondWith(
-    new Response("I'm the new <span class='a-winner-is-me'>response</span>", {
-        headers: {'Content-Type': 'text/html'}
-    })
-);
+    /* Hijacking requests */
+
+    // Respond with a html content type
+
+    // event.respondWith(
+    //     new Response("I'm the new <span class='a-winner-is-me'>response</span>", {
+    //         headers: {'Content-Type': 'text/html'}
+    //     })
+    // );
+
+    // console.log('event.request');
+
+    // Respond with gif on custom URL
+
+    // if (event.request.url.endsWith('.jpg')) {
+    //     event.respondWith(
+    //         fetch('/imgs/dr-evil.gif')
+    //     );
+    // }
 
 
-    console.log('hi user');
+    // respond on http status
+
+    event.respondWith(
+        fetch(event.request).then(function (response) {
+            if (response.status === 404) {
+
+                // return new Response('Something went wrong');
+
+                return fetch('/imgs/dr-evil.gif');
+            }
+            return response;
+        }).catch(function () {
+            return new Response('System is completely down, take a break!');
+        })
+    )
 });
